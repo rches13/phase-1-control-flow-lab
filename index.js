fc6573e4121211f7349878ea381489c2cf3f4ed3
Rodgers Chesoni <rodgerschesoni@gmail.com>
2024-06-25 10:13:28 +0300

    add control flow labs

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride <= 400) {
+    return 'This one is on me!';
+  } else if (ride > 2500) {
+    return 'No can do.';
+  } else if (ride > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (ride > 400 && ride <= 2000) {
+    return 'That will be twenty bucks.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let result = city === "NYC"? "Ok, sounds good.":"No go.";
+  return result;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(value){
+  switch (value) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you.";
+      break;
+    default:
+      return "Bye.";
+  }
 }
